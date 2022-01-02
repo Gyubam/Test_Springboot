67d619a2d760987d3d4f6f86c8552a182d725ddc
gyubum <sgb8170@naver.com>
2022-01-02 22:03:38 +0900

    initial commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // 테스트 커밋
+// 테스트 커밋2
 @Controller
 public class HelloController {
 
